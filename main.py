b825726e3d90074d90dbed28358043887b33a34f
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2020-12-08 00:00:17 +0200

    upload file set up

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template   
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = '../UPLOAD_FOLDER'
+
+app.config['MAX_CONTENT_PATH'] = 16 * 1024 * 1024
 
 @app.route("/")
 def home():
@@ -12,5 +17,16 @@ def home():
 def about():
     return render_template("about.html")
     
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
 if __name__ == "__main__":
     app.run(debug=True)
