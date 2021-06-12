6ede349e0b220c4baefdfb7e3ebd458d514969f5
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-06-12 23:51:50 +0200

    prep for twilio bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
@@ -10,8 +11,6 @@ app.config['MAX_CONTENT_PATH'] = 16 * 1024 * 1024
 @app.route("/")
 def home():
     return render_template("home.html")
-
-
 
 @app.route("/about")
 def about():
@@ -24,7 +23,11 @@ def features():
 @app.route('/upload')
 def upload_file():
    return render_template('upload.html')
-	
+
+
+@app.route('/api/bot')
+def bot():
+   return 'Hello I am going to be a bot'
 
 if __name__ == "__main__":
     app.run(debug=True)
